c65002a196342d844564e84ae569f6669386e5f8
K Smith <ksmith10@chicagobooth.edu>
2014-11-10 19:35:47 -0600

    homework_instagram
    KS

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,32 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @the_photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+
+    @the_caption = params[:the_caption]
+    @the_source = params[:the_source]
+
+  end
+
+  def destroy
+    @the_photo = Photo.find_by({ :id => params[:id] })
+    @the_photo.destroy
+
+  end
+
+  def edit_form
+    @the_photo = Photo.find(params[:id])
+  end
+
+  def update_row
+  end
 
 end
